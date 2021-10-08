1d2d74d4585b77fcd5a1dd62446731d471a33ea8
Temitope <ojotemitopeidnoble10@gmail.com>
2021-10-08 15:44:56 -0700

    aaaa

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,46 +1,46 @@
 import { ADD_TASK, DELETE_TASK, COMPLETE_TASK } from "./action";
 
-const initialData={
-    taskItems:[]
-}
+const initialData = {
+  taskItems: [],
+};
 
-const taskReducer =(state=initialData, action)=>{
-switch (action.type) {
-  case ADD_TASK:
-    return {
-      ...state,
-      taskItems: [
-        ...state.taskItems,
-        { text: action.payload, completed: false },
-      ],
-    };
+const taskReducer = (state = initialData, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return {
+        ...state,
+        taskItems: [
+          ...state.taskItems,
+          { text: action.payload, completed: false },
+        ],
+      };
 
-  case DELETE_TASK:
-    return {
-      ...state,
-      taskItems: [
-        ...state.taskItems.filter((task) => task.text !== action.payload),
-      ],
-    };
+    case DELETE_TASK:
+      return {
+        ...state,
+        taskItems: [
+          ...state.taskItems.filter((task) => task.text !== action.payload),
+        ],
+      };
 
-  case COMPLETE_TASK:
-        let updatedTaskItems = [];
-        state.taskItems.map((task) => {
-          if (task.text === action.payload) {
-            if (task.completed === true) {
-              task.completed = false;
-            } else {
-              task.completed = true;
-            }
+    case COMPLETE_TASK:
+      let updatedTaskItems = [];
+      state.taskItems.forEach((task) => {
+        if (task.text === action.payload) {
+          if (task.completed === true) {
+            task.completed = false;
+          } else {
+            task.completed = true;
           }
-          updatedTaskItems.push(task);
-        });
-    return {
-      ...state,
-      taskItems: updatedTaskItems
-    };
-  default:
-    return state;
-}
-}
-export default taskReducer
+        }
+        updatedTaskItems.push(task);
+      });
+      return {
+        ...state,
+        taskItems: updatedTaskItems,
+      };
+    default:
+      return state;
+  }
+};
+export default taskReducer;
